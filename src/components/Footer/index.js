b3bc53216d3c79732ccff6de31123df174ae8fa2
Erin Johnson <erinjohnson47@gmail.com>
2019-09-17 16:43:25 -0700

    format footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import img from './LAFPC_logo_1.png'
+import { 
+    // FooterLeft,
+    // FooterCenter,
+    // FooterRight,
+    FooterCopy,
+    Logo,
+    FooterDiv, 
+    SocialMediaDiv
+} from './style'
 
 const Footer = () => {
     return (
-        <div>
-            <hr/>
-            This is your footer...
-        </div>
+        <FooterDiv>
+            {/* <FooterLeft> */}
+                <Logo img src={img}></Logo>
+            {/* </FooterLeft> */}
+            {/* <FooterCenter> */}
+                <FooterCopy>
+                Copyright © 2019 Los Angeles Food Policy Council. All Rights Reserved
+                </FooterCopy>
+                <SocialMediaDiv></SocialMediaDiv>
+            {/* </FooterCenter> */}
+
+        </FooterDiv>
     )
 }
 
---
+++ src/components/Footer/style.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components'
+
+// export const FooterLeft = styled.div`
+//     display: flex;
+//     justify-content: center;
+//     width: 25%
+// `
+// export const FooterCenter = styled.div`
+//     width: 50%
+// `
+// export const FooterRight = styled.div`
+//     width: 25%
+// `
+
+export const Logo = styled.img`
+    height: 75px;
+    width: 75px;
+    padding: 12px 94px;    
+`
+export const FooterCopy = styled.div`
+    ${'' /* position: absolute; */}
+    left: 27.5%;
+    right: 27.43%;
+    top: 37%;
+    bottom: 36%;
+    font-family: Avenir Next;
+    font-size: 20px;
+    line-height: 27px;
+    padding: 25px auto;
+    color: #FFFFFF;
+` 
+export const FooterDiv = styled.div`
+    display: flex;
+    justify-content: space-around;
+    background-color: #156574
+    height: 100px;
+`
+export const SocialMediaDiv = styled.div`
+
+`
