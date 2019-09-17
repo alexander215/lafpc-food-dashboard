f4ac4ffaaa366b51551964fe6ae43d12be114bfa
Narek <narex111@gmail.com>
2019-09-16 19:27:59 -0700

    display database

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import firebase from 'firebase';
+import Table from "./components/Table"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    firebase.initializeApp({
+      apiKey: "AIzaSyDotqscj0pdXzEKwC0p44dT2vYtITVD6Y8",
+      authDomain: "lafpc-food-dashboard.firebaseapp.com",
+      databaseURL: "https://lafpc-food-dashboard.firebaseio.com",
+      projectId: "lafpc-food-dashboard",
+      storageBucket: "",
+      messagingSenderId: "893039884595",
+      appId: "1:893039884595:web:f2d0987d63330d530ea646"
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <div>
+          <h1>App</h1>
+          <Table db={firebase}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Table/index.js
@@ -0,0 +1,37 @@
+
+import React, {Component} from 'react';
+
+class Table extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      masterSheet: []
+    };
+  }
+
+
+  componentDidMount(){
+    console.log(this.props, 'this.props in componentDidMount')
+    const msRef = this.props.db.database().ref().child("masterSheet")
+    const childRef = msRef.child("0")
+    console.log(childRef, 'childRef in componentDidMount')
+    childRef.on("value", snapshot=>{
+      console.log(snapshot, "snpashot")
+      this.setState({
+        masterSheet: snapshot.val()
+      })
+    })
+  }
+
+  render(){
+    console.log(this.props.db)
+    console.log(this.state.masterSheet)
+    return(
+      <div>
+        {this.state.masterSheet}
+      </div>
+    )
+  }
+}
+
+export default Table
