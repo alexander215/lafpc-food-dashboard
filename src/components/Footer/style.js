3e6960b926ca9ee788d603eaf70a47ae9e3ae34f
Erin Johnson <erinjohnson47@gmail.com>
2019-09-17 18:51:13 -0700

    update padding around footer divs

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -1,51 +1,36 @@
 import styled from 'styled-components'
 
-// export const FooterLeft = styled.div`
-//     display: flex;
-//     justify-content: center;
-//     width: 25%
-// `
-// export const FooterCenter = styled.div`
-//     width: 50%
-// `
-// export const FooterRight = styled.div`
-//     width: 25%
-// `
-
-export const Logo = styled.img`
-    height: 75px;
-    width: 75px;
-    padding: 12px 94px;    
-`
-export const FooterCopy = styled.div`
-    {'' /* left: 27.5%;
-    right: 27.43%;
-    top: 37%;
-    bottom: 36%; */}
-    font-family: Avenir Next;
-    font-size: 20px;
-    line-height: 27px;
-    padding: 25px 94px;
-    color: #FFFFFF;
-` 
 export const FooterDiv = styled.div`
     display: flex;
     justify-content: space-around;
     background-color: #156574
     height: 100px;
 `
+export const Logo = styled.img`
+    height: 75px;
+    width: 75px;
+    padding: 12px 0px;    
+`
+export const FooterCopy = styled.div`
+    font-family: Avenir Next;
+    font-size: 20px;
+    line-height: 27px;
+    padding: 37px 10px;
+    color: #FFFFFF;
+` 
 export const SocialMediaDiv = styled.div`
-    cursor: pointer;
-    flex-direction: row;
-    justify-content: space-between;
-    X: 5px;
-    Y: 5px;
-    W: 30px;
-    H: 30px;
-    X Constraint: Scale;
-    Y Constraint: Scale;
+    justify-content: center;
+    padding: 35px 35px;
+    width: 300px;
+    height: 100px;
 `
 export const SocialMediaImg = styled.a`
+    cursor: pointer;
+    padding: 35px 10px;
     width: 30px;
     height: 30px;
+    X: 5px;
+    Y: 5px;
+    X Constraint: Scale;
+    Y Constraint: Scale;
 `
